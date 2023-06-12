67d0b7f8f432399065cee8690292a4892fd9db72
MaksMaryanchuk <64914675+maryanchuk04@users.noreply.github.com>
2023-06-12 14:04:47 +0300

    Cors update

--- Galery/Program.cs
+++ Galery/Program.cs
@@ -27,6 +27,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x =>
+{
+    x.AllowAnyMethod()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed((origin) => true)
+        .AllowCredentials();
+});
+
 app.UseHttpsRedirection();
 
 #endregion
